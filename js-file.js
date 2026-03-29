94416b2c36e89d5b88ea4bae35a3b194c36ca234
akramshaheen <akram.shaheen27@gmail.com>
2026-03-29 12:56:30 +0100

    chore: improve readability by cleaning code

--- js-file.js
+++ js-file.js
@@ -7,6 +7,12 @@ let op = "";
 let result;
 let resultIsShown = false;
 
+function updateNumbers() {
+  if (op === "") num1 = display.textContent;
+  if (op !== "") num2 = display.textContent;
+}
+
+//Calculation functions
 function add(x, y) {
   return x + y;
 }
@@ -28,20 +34,23 @@ function power(x, y) {
   return x ** y;
 }
 
-function decimal() {
-  const lastChar = display.textContent.slice(-1);
+function operate(num1, op, num2) {
+  num1 = +num1;
+  num2 = +num2;
 
-  if (lastChar === "." || display.textContent.includes(".")) return;
+  if (op === "+") result = add(num1, num2);
+  if (op === "-") result = subtract(num1, num2);
+  if (op === "x") result = multiply(num1, num2);
+  if (op === "/") result = divide(num1, num2);
+  if (op === "^") result = power(num1, num2);
 
-  if (lastChar === "") {
-    display.append("0.");
-    if (op !== "") num2 = display.textContent;
-    return;
-  }
-
-  display.append(".");
+  if (result === "ERROR") return "ERROR";
+  if (result >= 1e14 || result <= -1e14) return result.toExponential(2);
+  if (!Number.isInteger(result)) return Math.floor(result * 1e14) / 1e14;
+  return result;
 }
 
+//Button callbacks
 function clear() {
   num1 = "";
   num2 = "";
@@ -71,55 +80,70 @@ function plusMinus() {
   if (resultIsShown) result = display.textContent;
 }
 
-function operate(num1, op, num2) {
-  num1 = +num1;
-  num2 = +num2;
+function decimal() {
+  const lastChar = display.textContent.slice(-1);
 
-  if (op === "+") result = add(num1, num2);
-  if (op === "-") result = subtract(num1, num2);
-  if (op === "x") result = multiply(num1, num2);
-  if (op === "/") result = divide(num1, num2);
-  if (op === "^") result = power(num1, num2);
+  if (lastChar === "." || display.textContent.includes(".")) return false;
 
-  if (result === "ERROR") return "ERROR";
-  if (result >= 1e14 || result <= -1e14) return result.toExponential(2);
-  if (!Number.isInteger(result)) return Math.floor(result * 1e14) / 1e14;
-  return result;
+  if (lastChar === "") {
+    display.append("0.");
+    return true;
+  }
+
+  display.append(".");
+  return true;
 }
 
+function numberEvent(e) {
+  if (resultIsShown) {
+    clear();
+  }
+
+  if (num2 === "" && op !== "") display.textContent = "";
+
+  if (display.textContent.length >= 16) return;
+
+  if (e.target.dataset.value === ".") {
+    if (decimal()) return updateNumbers();
+  }
+
+  display.append(e.target.dataset.value);
+  updateNumbers();
+}
+
+function operatorEvent(e) {
+  if (num1 !== "" && num2 !== "" && !resultIsShown) {
+    display.textContent = operate(num1, op, num2);
+    num1 = display.textContent;
+    num2 = "";
+  }
+
+  if (resultIsShown) {
+    num1 = display.textContent;
+    num2 = "";
+    resultIsShown = false;
+  }
+
+  if (num1 === "") return;
+
+  op = e.target.dataset.value;
+}
+
+function equals() {
+  if (num2 === "") return;
+
+  display.textContent = operate(num1, op, num2);
+  resultIsShown = true;
+}
+
+//Event handler
 buttons.addEventListener("click", (e) => {
   if (e.target.dataset.type === "number") {
-    if (resultIsShown) {
-      clear();
-    }
-
-    if (num2 === "" && op !== "") display.textContent = "";
-
-    if (display.textContent.length >= 16) return;
-
-    if (e.target.dataset.value === ".") return decimal();
-
-    display.append(e.target.dataset.value);
-    if (op === "") num1 = display.textContent;
-    if (op !== "") num2 = display.textContent;
+    numberEvent(e);
   }
 
   if (e.target.dataset.type === "operator") {
-    if (num1 !== "" && num2 !== "" && !resultIsShown) {
-      display.textContent = operate(num1, op, num2);
-      num1 = display.textContent;
-      num2 = "";
-    }
-
-    if (resultIsShown) {
-      num1 = display.textContent;
-      num2 = "";
-      resultIsShown = false;
-    }
-
-    if (num1 === "") return;
-
-    op = e.target.dataset.value;
+    operatorEvent(e);
   }
 
   if (e.target.matches(".delete")) {
@@ -135,9 +159,6 @@ buttons.addEventListener("click", (e) => {
   }
 
   if (e.target.matches(".equals")) {
-    if (num2 === "") return clear();
-
-    display.textContent = operate(num1, op, num2);
-    resultIsShown = true;
+    equals();
   }
 });
